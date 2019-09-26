70281d847a8c52c6109704f4cbf11a1e550b8f61
Zachary <zacarlin@gmail.com>
2019-09-25 23:03:51 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myDotnetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("One must learn to make a Hello World app");
+            Console.WriteLine("Or did they really learn the language");
+
+        }
+    }
+}
